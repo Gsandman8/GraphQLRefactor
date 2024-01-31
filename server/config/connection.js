b3d564cb5e226c1f086261f1af9afb47c80e416f
Gsandman8 <gage.e.sandman@gmail.com>
2024-01-30 16:14:50 -0800

    Another connection file change

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const connection = mongoose.createConnection(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000
+});
 
 module.exports = connection;
